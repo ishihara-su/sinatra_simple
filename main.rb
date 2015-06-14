77ccf77a2d875722d4a76713995e5a002a5067f6
Susumu Ishihara <ishihara.susumu@shizuoka.ac.jp>
2015-06-14 10:21:42 -0700

    クライアントからのテキストをポストできるようにした。

--- main.rb
+++ main.rb
@@ -4,10 +4,18 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  p params["ex_text"]
+end
+
 get '/hello' do
   "Hello world"
 end
 
 get '/example' do
   erb :example
+end
+
+not_found do
+    "Not Found: The requested URL was not found on this server."
 end
